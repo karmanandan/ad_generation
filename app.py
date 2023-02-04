6a76bc537d5e85b627fef7adf4b700116e575649
raghunandan.karma <30720494+karmanandan@users.noreply.github.com>
2023-02-04 13:21:33 +0530

    adding image templates

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import streamlit as st
 import openai
 import random
+import pandas as pd
+from csv import writer
+from csv import DictWriter
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw 
+
 
 st.title("Ad cre'AI'tor")
+
 
 # product info
 product_prompt = st.text_input("enter product info")
@@ -20,32 +28,117 @@ features_prompt = st.text_area(
 )
 
 # api key
-openai.api_key = "" # enter your api key here
+openai.api_key =  # enter your api key here
 
 
 def add_creator():
     indentation_op = []
-    for i in range(5):
+    tagline_list = []
+    for _ in range(2):
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=f"Write a creative ad for the following product to run on Facebook,instagram and linkedin aimed at {audience_prompt}:\n\
-            \nProduct: {product_prompt} with product name {product_name_prompt} {features_prompt}",
-            #   Learning Room is a virtual environment to help students from kindergarten to high school excel in school.
+            # prompt=f"Write a creative ad for the following product to run \
+            #     on Facebook,instagram aimed at {audience_prompt}:\n\
+            # \nProduct: product features were{product_prompt} with product name {product_name_prompt} \
+            # {features_prompt}",
+            prompt = f"For the following product {product_name_prompt}, \
+             create an advertisement to run on Facebook and Instagram with a focus on {audience_prompt}. \
+                Product: {product_name_prompt} product qualities were {product_prompt} with the product name {product_name_prompt}",
             temperature=0.5,
             max_tokens=100,
             top_p=1.0,
             frequency_penalty=0.0,
             presence_penalty=0.0,
         )
-        # return response['choices'][0]['text']
+        tagline_respone = openai.Completion.create(
+            model="text-davinci-003",
+            # prompt=f"write a tagline with {product_prompt, product_name_prompt} ",
+            prompt=f"write a tagline for {product_name_prompt} with following information {product_prompt} ",
+            temperature=0.5,
+            max_tokens=256,
+            top_p=1,
+            best_of=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+        )
         indentation_op.append(response["choices"][0]["text"])
-    print(indentation_op)
-    return indentation_op
-
+        tagline_list.append(tagline_respone["choices"][0]["text"])
+    return indentation_op, tagline_list
 
 with st.container():
     if st.button("Generate ad"):
-        st.write(add_creator())
+        output_generations = add_creator()
+
+        st.write("Taglines:")
+        st.write(output_generations[1])
+        st.write("Ads:")
+        st.write(output_generations[0])
+
+
+        # image
+        # def write_text_on_images(img, text):
+        #     final_res = []
+        #     draw = ImageDraw.Draw(img)
+        #     font = ImageFont.truetype("arial.ttf", 20)  # load font
+        #     W,H = img.size
+        #     _, _, w, h = draw.textbbox((0, 0), text, font=font)
+        #     new_width = (W - w) / 2
+        #     new_height = (H - h) / 2
+        #     final_result = draw.text((new_width,new_height), text.strip('"'), font=font, fill=(113, 166, 210)) #(200, 200, 0)
+        #     final_res.append(final_result)
+        #     return final_res
+        
+        img1_bg = Image.open("bg_img.jpeg")
+        text_1 =  output_generations[1][0]
+        draw = ImageDraw.Draw(img1_bg)
+        font = ImageFont.truetype("arial.ttf", 20)  # load font
+        W,H = img1_bg.size
+        _, _, w, h = draw.textbbox((0, 0), text_1, font=font)
+        new_width = (W - w) / 2
+        new_height = (H - h) / 2
+        draw.text((new_width,new_height), text_1.strip('"'), font=font, fill=(113, 166, 210)) #(200, 200, 0)
+
+        # img_1= write_text_on_images(img1_bg, text_1)
+
+        img2_bg = Image.open("img_2.jpeg")
+        text_2 =  output_generations[1][1]
+        draw = ImageDraw.Draw(img2_bg)
+        font = ImageFont.truetype("arial.ttf", 20)  # load font
+        W,H = img1_bg.size
+        _, _, w, h = draw.textbbox((0, 0), text_2, font=font)
+        new_width = (W - w) / 2
+        new_height = (H - h) / 2
+        draw.text((new_width,new_height), text_2.strip('"'), font=font, fill=(0, 0, 0)) #(200, 200, 0)
+
+        st.image([img1_bg,img2_bg], caption=["Template images for taglines","2Img"])
+
+        generation_dict = {
+            "ProductInfo": product_prompt,
+            "ProductName": product_name_prompt,
+            "Audience": audience_prompt,
+            "Features": features_prompt,
+            "Generations": output_generations[1],
+            "Tagline": output_generations[0],
+        }
+
+        field_names = [
+            "ProductInfo",
+            "ProductName",
+            "Audience",
+            "Features",
+            "Generations",
+            "Tagline",
+        ]
+        # Create a file object for this file
+        with open("event.csv", "a") as f_object:
+            # You will get a object of DictWriter
+            dictwriter_object = DictWriter(f_object, fieldnames=field_names)
+
+            # Pass the dictionary as an argument to the Writerow()
+            dictwriter_object.writerow(generation_dict)
+
+            # Close the file object
+            f_object.close()
 
         # Add a next button and a previous button
         # page_number = 0
